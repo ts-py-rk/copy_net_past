456f0e916ac4cc147baca83b529e25bba5e5aa61
tspyrk <krypst@gmail.com>
2022-05-17 00:25:55 +0500

    client.py: 	- оформление¸ server.py: 	- не вылетает при отправке 	- оформление

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
-import pyperclip, time, socket, pickle, threading, sys, select
+import pyperclip
+import time
+import socket
+import pickle
+import threading
+import sys
+import select
 
-#                                           # функция обертка для запуска потоков
+
+# функция обертка для запуска потоков
 def thread(my_func):
     def wrapper(*args, **kwargs):
         my_thread = threading.Thread(target=my_func, args=args, kwargs=kwargs)
         my_thread.start()
     return wrapper
 
-#                                           # функция подключения к указанному серверу
-def input_server(ip, port, sec):
 
+# функция подключения к указанному серверу
+def input_server(ip, port, sec):
     ip_def = ip
     port_def = port
-    print(f'Начинается автоматическое подключение к {ip}:{port}\nНажмите Enter для ручной настройки подключения в течении {sec} секунд)')
-
+    print(f'Начинается автоматическое подключение к {ip}:{port}\nНажмите '
+          f'Enter для ручной настройки подключения в течении {sec} секунд)')
     rlist, _, _ = select.select([sys.stdin], [], [], sec)
     if rlist:
         ip = sys.stdin.readline()
@@ -32,13 +39,14 @@ def input_server(ip, port, sec):
         else:
             ip = ip_def
             port = port_def
-
     global server, sor
     server = ip, port
     sor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sor.bind(('', 0))
 
-@thread                                     # функция отправки буфера на сервер
+
+# функция отправки буфера на сервер
+@thread
 def send_bufer(second):
     local = threading.local()
     local.local_bufer = set()
@@ -50,12 +58,14 @@ def send_bufer(second):
             local.add_len = len(local.local_bufer)
         if local.start_len != local.add_len:
             print('Send to server', local.local_bufer_now)
-            local.pack = pickle.dumps(local.local_bufer_now, protocol=4)
+            local.pack = pickle.dumps(local.local_bufer_now, protocol=2)
             sor.sendto(local.pack, server)
             local.local_bufer = local.local_bufer & local.local_bufer_now
         time.sleep(second)
 
-@thread                                     # функция бесконечного цикла приема сетевого буфера
+
+# функция бесконечного цикла приема сетевого буфера
+@thread
 def net_in_loc():
     while True:
         data = sor.recv(1024)
@@ -66,18 +76,17 @@ def net_in_loc():
                 pyperclip.copy(i)
         time.sleep(1)
 
-###################################################################################################
 
-print('Start client')                       # стартовое приветствие
+start_message = 'Start client'
+server_ip = '192.168.88.210'
+server_port = 666
+period = 3
+
+print(start_message)
 hello = socket.gethostbyname(socket.gethostname())
-pyperclip.copy(hello)                       # стартовый пакет c ip клиента
-
-input_server('localhost', 666, 3)           # подключаемся к указанному серверу
-
-send_bufer(3)                               # запуск потока отправки буфера на сервер
-
-bufer = set()                               # создание множества для буфера
-bufer.add(pyperclip.paste())                # добавляем во множество буфер обмена
-
-net_in_loc()                                # запускаем  в потоке
-
+pyperclip.copy(hello)                           # стартовый пакет c ip клиента
+input_server(server_ip, server_port, period)    # подключаемся к серверу
+send_bufer(period)                              # запуск потока отправки буфера
+bufer = set()                                   # создание множества для буфера
+bufer.add(pyperclip.paste())                    # добавляем во множество буфер
+net_in_loc()                                    # запускаем  в потоке
--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-import socket, time, pickle, select, sys
+import socket
+import time
+import pickle
+import select
+import sys
+
 
 def start_server(port_def, sec):
     ip = socket.gethostbyname(socket.gethostname())
@@ -23,6 +28,7 @@ def start_server(port_def, sec):
         else:
             port = port_def
 
+
 start_server(666, 3)
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('', port))
@@ -31,7 +37,13 @@ print('\nЖдем подключения клиентов\n')
 
 while 1:
     data, addres = sock.recvfrom(1024)
-    pack = pickle.loads(data)
+    try:
+        pack = pickle.loads(data)
+        print(f'{data = }')
+    except:
+        data = b'\x80\x02c__builtin__\nset\nq\x00]q\x01X\x0c\x00\x00\x00' \
+               b'Error X_X ((q\x02a\x85q\x03Rq\x04.'
+        pack = pickle.loads(data)
     print(addres, 'send:', pack)
     if addres not in clients:
         print('Connecting client ', addres)
@@ -40,5 +52,5 @@ while 1:
     for client in clients:
         if client != addres:
             sock.sendto(data, client)
-            print('Send to',client, ':', pickle.loads(data))
+            print('Send to', client, ':', pickle.loads(data))
     time.sleep(1)
